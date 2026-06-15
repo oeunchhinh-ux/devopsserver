7fd119fd1acaff40fa5209570b0153bd6aa07059
Chhinh <mrbob101signal@gmail.com>
2026-06-15 09:20:33 +0700

    remove text

--- src/app/items.js
+++ src/app/items.js
@@ -1,7 +1,7 @@
 export const items = [
   {
     id: 1,
-    item_name: "Aula Wireless Mouse by chhinh",
+    item_name: "Aula Wireless Mouse",
     item_description:
       "Ergonomic wireless mouse with adjustable DPI and silent clicks.",
     item_price: 19.99,
