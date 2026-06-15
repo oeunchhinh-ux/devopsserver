e0487962f28cae07005b85b48e42609e13bce4c0
SENGMEY7777 <vannsengmey748@gmail.com>
2026-06-15 13:19:58 +0300

    modify and edit product

--- src/app/items.js
+++ src/app/items.js
@@ -1,7 +1,7 @@
 export const items = [
   {
     id: 1,
-    item_name: "Aula Wireless Mouse",
+    item_name: "Sengmey Koko",
     item_description:
       "Ergonomic wireless mouse with adjustable DPI and silent clicks.",
     item_price: 19.99,
