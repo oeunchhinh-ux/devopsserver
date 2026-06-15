207a9cf21c5dd1942da98e908ef5a1928340aecb
Chhinh <mrbob101signal@gmail.com>
2026-06-15 09:49:00 +0700

    update logo photo

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar({ searchBar }) {
       <nav className="w-full bg-[#0f1216] flex items-center justify-between px-4 py-2 shadow-[0_4px_6px_rgba(0,0,0,0.5)]">
         <div className="flex items-center gap-3 h-15">
           <img
-            src="https://coin-images.coingecko.com/coins/images/66868/large/1hz8mg7dk625tp1kzy43cq4gtpsp.?1750886735"
+            src="https://i.pinimg.com/736x/02/65/7a/02657a618b637e27942697e90567b686.jpg"
             alt="avatar"
             className="w-15 h-15 rounded-full object-cover border-2 border-white"
           />
